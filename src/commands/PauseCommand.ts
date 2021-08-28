d6717b06cd9f832f4835328d2265698066b11acb
Rohan Kumar <rohan.shuvam@gmail.com>
2021-08-28 18:37:45 +0530

    Complete music command commit

---
+++ src/commands/PauseCommand.ts
@@ -0,0 +1,26 @@
+import { BaseCommand } from "../Utils/structures/BaseCommand";
+import DiscordClient from "../Client/Client";
+import { CommandInteraction, MessageEmbed } from "discord.js";
+import { checkMusicPermission } from "../Utils/functions";
+
+export default class PauseCommand extends BaseCommand {
+    constructor() {
+        super("pause", "pause the song");
+    }
+    async run(client: DiscordClient, interaction: CommandInteraction) {
+        if (!(await checkMusicPermission(client, interaction))) return;
+        const { player } = client.players.get(interaction.guildId);
+        if (player.paused) {
+            const embed = new MessageEmbed()
+                .setColor("#FFBD4F")
+                .setDescription("Player is already paused");
+            await interaction.reply({ embeds: [embed], ephemeral: true });
+        } else {
+            player.pause(true);
+            const embed = new MessageEmbed()
+                .setColor("#FFBD4F")
+                .setDescription("Player Paused");
+            await interaction.reply({ embeds: [embed] });
+        }
+    }
+}
