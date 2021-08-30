3a1510c6484080686f4583ac6e143dfcc1e8c87f
Rohan Kumar <rohan.shuvam@gmail.com>
2021-08-30 15:53:02 +0530

    8D filter command

---
+++ src/commands/EightDCommand.ts
@@ -0,0 +1,26 @@
+import { BaseCommand } from "../Utils/structures/BaseCommand";
+import DiscordClient from "../Client/Client";
+import { CommandInteraction, MessageEmbed } from "discord.js";
+import { checkMusicPermission } from "../Utils/functions";
+
+export default class EightDCommand extends BaseCommand {
+    constructor() {
+        super("8d", "Apply 8D Filter");
+    }
+    async run(client: DiscordClient, interaction: CommandInteraction) {
+        if (!(await checkMusicPermission(client, interaction))) return;
+        const { player } = client.players.get(interaction.guildId);
+        if (player.eightD) {
+            const embed = new MessageEmbed()
+                .setColor("#FFBD4F")
+                .setDescription("8D filter Deactivated");
+            await interaction.reply({ embeds: [embed] });
+        } else {
+            const embed = new MessageEmbed()
+                .setColor("#FFBD4F")
+                .setDescription("8D Filter Activated");
+            await interaction.reply({ embeds: [embed] });
+        }
+        player.eightD = !player.eightD;
+    }
+}
