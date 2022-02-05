677d86c04126c95bc782d19d6aa615730e2c84ff
Rohan Kumar <rohan.shuvam@gmail.com>
2022-02-05 13:33:07 +0530

    fixed remove command

--- src/commands/RemoveCommand.ts
+++ src/commands/RemoveCommand.ts
@@ -2,6 +2,7 @@ import { CommandInteraction, MessageEmbed } from "discord.js";
 import DiscordClient from "../Client/Client";
 import { BaseCommand } from "../Utils/structures/BaseCommand";
 import { checkMusicPermission } from "../Utils/functions";
+import { Track } from "erela.js/structures/Player";
 
 export default class RemoveCommand extends BaseCommand {
     constructor() {
@@ -19,7 +20,14 @@ export default class RemoveCommand extends BaseCommand {
             interaction.editReply({ embeds: [embed] }).catch(console.error);
             return;
         }
-        const song = player.queue[position - 1];
+        const song = player.queue[position-1] as Track;
+        console.log(song)
+        if(!song){
+            const embed = new MessageEmbed().setColor("#FFBD4F").setDescription('Songs you are trying to remove do not exist')
+            interaction.editReply({ embeds: [embed] }).catch(console.error);
+            return;
+        }
+
         player.queue.remove(position);
         const embed = new MessageEmbed()
             .setColor("#FFBD4F")
