fe2d862372372fa91cda635c522d55abc370fcd4
Rohan Kumar <rohan.shuvam@gmail.com>
2022-02-08 19:38:52 +0530

    Added pause and remuse based on Voice Activity to save resources

--- app/src/main/java/me/rohank05/beat/Listeners.java
+++ app/src/main/java/me/rohank05/beat/Listeners.java
@@ -2,10 +2,13 @@ package me.rohank05.beat;
 
 import com.jagrosh.jdautilities.command.Command;
 import me.duncte123.botcommons.BotCommons;
+import me.rohank05.beat.lavaplayer.PlayerManager;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.channel.voice.GenericVoiceChannelEvent;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceLeaveEvent;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.Interaction;
@@ -31,7 +34,7 @@ public class Listeners extends ListenerAdapter {
         LOGGER.info("{} Ready!!", jda.getSelfUser().getAsTag());
         Guild guild = jda.getGuildById("939760380719292416");
         CommandListUpdateAction commands = guild.updateCommands();
-        List<CommandPrivilege> permissions =new ArrayList<>();
+        List<CommandPrivilege> permissions = new ArrayList<>();
         permissions.add(CommandPrivilege.enableUser("687893451534106669"));
         commands.addCommands(new CommandData("play", "Play or add songs to the queue").addOption(OptionType.STRING, "song-or-playlist", "Song Name or Song URL or Playlist URL", true).setDefaultEnabled(true));
         commands.addCommands(new CommandData("join", "Join the Voice Channel, You are in").setDefaultEnabled(true));
@@ -48,7 +51,34 @@ public class Listeners extends ListenerAdapter {
     }
 
     @Override
-    public void onGenericVoiceChannel(@NotNull GenericVoiceChannelEvent event) {
+    public void onGuildVoiceLeave(@NotNull GuildVoiceLeaveEvent event) {
+        /**
+         * Check if Bot has left the channel. If it had it will destroy the player
+         */
+        if (!event.getGuild().getSelfMember().getVoiceState().inVoiceChannel())
+            if (PlayerManager.getINSTANCE() != null)
+                if (PlayerManager.getINSTANCE().getMusicManager(event.getGuild()).audioPlayer.getPlayingTrack() != null)
+                    PlayerManager.getINSTANCE().getMusicManager(event.getGuild()).audioPlayer.destroy();
 
+        /**
+         * Check if is the only member left in the channel. If yes then it will pause the song until another member join
+         */
+        if (event.getChannelLeft().getMembers().size() == 1)
+            if (event.getChannelLeft().getMembers().get(0).equals(event.getGuild().getSelfMember()))
+                if (PlayerManager.getINSTANCE() != null)
+                    PlayerManager.getINSTANCE().getMusicManager(event.getGuild()).audioPlayer.setPaused(true);
+    }
+
+    @Override
+    public void onGuildVoiceJoin(@NotNull GuildVoiceJoinEvent event) {
+
+        /**
+         * Resumes the player when another users join
+         */
+        if (event.getChannelJoined().getMembers().contains(event.getGuild().getSelfMember()))
+            if (event.getChannelJoined().getMembers().size() > 1)
+                if (PlayerManager.getINSTANCE() != null)
+                    if (PlayerManager.getINSTANCE().getMusicManager(event.getGuild()).audioPlayer.isPaused())
+                        PlayerManager.getINSTANCE().getMusicManager(event.getGuild()).audioPlayer.setPaused(false);
     }
 }
