8f1d5318ba96305b2f272ef05ef57397891f0a20
Rohan Kumar <rohan.shuvam@gmail.com>
2022-02-09 19:10:05 +0530

    Added spotify support

---
+++ app/src/main/java/me/rohank05/beat/lavaplayer/sourcemod/TrackNotFoundException.java
@@ -0,0 +1,5 @@
+package me.rohank05.beat.lavaplayer.sourcemod;
+
+public class TrackNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 6550093849278285754L;
+}
