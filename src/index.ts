b22bab85344f2aa087f8f0a0767c943f262fb0d5
NORO3618 <66942330+Noro95@users.noreply.github.com>
2022-02-18 19:31:27 +0200

    uncaughtException event

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,31 @@ import DiscordClient from "./Client/Client";
 import { registerCommands, registerEvents } from "./Utils/registry";
 import { connect } from "mongoose";
 import { setSongCount } from "./Utils/database/functions";
+import { MessageEmbed, WebhookClient } from "discord.js";
 require("dotenv").config();
 const client = new DiscordClient({
     intents: ["GUILDS", "GUILD_VOICE_STATES"],
+});
+const err_log_webhook = new WebhookClient({
+    url: process.env.ERROR_LOG_WEBHOOK!,
+});
+
+process.on("uncaughtException", function (error) {
+    console.log("Uncaught Exception:", error);
+    const errmsg = error.message;
+    err_log_webhook.send({
+        embeds: [
+            new MessageEmbed().setDescription(
+                "UNCAUGHT: " +
+                    errmsg.slice(
+                        0,
+                        errmsg.length >= 800
+                            ? 800 - errmsg.length
+                            : errmsg.length
+                    )
+            ),
+        ],
+    });
 });
 
 connect(process.env.MONGO_URL!)
