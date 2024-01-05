e1190ac7bd982d3ac4df055b7a74332bddd5d927
Rohan Kumar <rohan.shuvam@gmail.com>
2024-01-05 23:12:22 +0530

    Removed Old Code

--- src/Utils/database/SongSchema.ts
+++
@@ -1,8 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const SongSchema = new Schema({
-    bot_id: String,
-    count: Number,
-});
-
-export const song = model("song", SongSchema);
--- src/Utils/structures/BaseEvent.ts
+++
@@ -1,9 +0,0 @@
-import DiscordClient from "../../Client/Client";
-
-export abstract class BaseEvent {
-    constructor(private name: string) {}
-    getName(): string {
-        return this.name;
-    }
-    abstract run(client: DiscordClient, ...args: any): void;
-}
